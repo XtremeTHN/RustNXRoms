58c3d7815296c426ad5f769fcfd8da0c1e4bd257
Axel <nigthmaresans2@gmail.com>
2026-03-14 14:16:31 -0600

    feat: cnmt header support

---
+++ roms/src/formats/cnmt.rs
@@ -0,0 +1,43 @@
+use binrw::BinRead;
+
+#[derive(BinRead, Debug)]
+#[br(little, repr = u8)]
+pub enum ContentMetaType {
+    Invalid = 0,
+    SystemProgram = 0x01,
+    SystemData = 0x02,
+    SystemUpdate = 0x03,
+    BootImagePackage = 0x04,
+    BootImagePackageSafe = 0x05,
+    Application = 0x80,
+    Patch = 0x81,
+    AddOnContent = 0x82,
+    Delta = 0x83,
+    DataPatch = 0x84
+}
+
+#[derive(BinRead, Debug)]
+#[br(little, repr = u8)]
+pub enum ContentMetaAttributes {
+    IncludesExFatDriver = 0,
+    Rebootless = 1,
+    Compacted = 2,
+    ProperProgramExists = 3,
+    _4 = 4,
+    _5 = 5
+}
+
+#[derive(BinRead, Debug)]
+#[br(little)]
+pub struct PackagedContentMetaHeader {
+    pub id: u64,
+    pub version: u32,
+    pub content_meta_type: ContentMetaType,
+    #[br(pad_before = 0x1)]
+    pub ext_header_size: u16,
+    pub content_count: u16,
+    pub content_meta_count: u16,
+    pub content_meta_attributes: ContentMetaAttributes,
+    #[br(pad_before = 0x3, pad_after = 0x4)]
+    pub req_sys_version: u32
+}
--- roms/src/formats/mod.rs
+++ roms/src/formats/mod.rs
@@ -1,4 +1,4 @@
 pub mod nca;
-// pub mod nsp;
 pub mod xci;
 pub mod nacp;
+pub mod cnmt;
